9e17ccac13cfae45d36bc1406573438814134503
Kaloyan Kulov <kaloyankulov2003kk@gmail.com>
2023-08-12 14:50:00 +0300

    Updated UI tests URL

--- tests/ui/boardgames.test.js
+++ tests/ui/boardgames.test.js
@@ -1,8 +1,9 @@
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test('Check boardgames page', async ({ page }) => {
-    await page.goto('http://localhost:8080/boardgames');
-    const list = await page.$('ul');
-    expect(list).toBeTruthy();
-  });
-  
+test("Check boardgames page", async ({ page }) => {
+  await page.goto(
+    "https://kaloyan-123-boardgames-deployment.onrender.com/boardgames"
+  );
+  const list = await page.$("ul");
+  expect(list).toBeTruthy();
+});
